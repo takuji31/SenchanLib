8e68dbd224274402e24583bcc9c887364a8c3b9d
Nishibayashi Takuji <takuji@senchan.jp>
2012-03-09 08:39:45 +0900

    ActionBarSherlockのFragmentを継承するように

--- src/jp/senchan/lib/ui/BaseListFragment.java
+++ src/jp/senchan/lib/ui/BaseListFragment.java
@@ -1,31 +1,32 @@
 package jp.senchan.lib.ui;
 
+import com.actionbarsherlock.app.SherlockListFragment;
+
 import jp.senchan.lib.BaseApp;
-import android.support.v4.app.ListFragment;
 import android.widget.Toast;
 
-public class BaseListFragment extends ListFragment {
-	
+public class BaseListFragment extends SherlockListFragment {
+
 	public BaseApp app() {
 		return getBaseActivity().app();
 	}
-	
+
 	public BaseActivity getBaseActivity() {
 		return (BaseActivity) getActivity();
 	}
-	
+
 	public Toast toast(int text_id) {
 		return app().toast(text_id);
 	}
-	
+
 	public Toast toast(String text) {
 		return app().toast(text);
 	}
-	
+
 	public Toast longToast(int text_id) {
 		return app().longToast(text_id);
 	}
-	
+
 	public Toast longToast(String text) {
 		return app().longToast(text);
 	}
