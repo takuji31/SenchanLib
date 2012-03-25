499fcf394a10556c792abcf0932251065448aa92
Nishibayashi Takuji <takuji@senchan.jp>
2012-03-26 07:25:12 +0900

    SDK11以降のAsyncTaskの実行方法の差異を吸収してくれるクラスを追加

---
+++ src/jp/senchan/lib/os/AsyncTaskCompat.java
@@ -0,0 +1,15 @@
+package jp.senchan.lib.os;
+
+import android.os.AsyncTask;
+import android.os.Build;
+
+public abstract class AsyncTaskCompat<Params, Progress, Result> extends AsyncTask<Params, Progress, Result> {
+
+    public AsyncTask<Params,Progress,Result> threadExecute(Params... params) {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            return this.executeOnExecutor(THREAD_POOL_EXECUTOR, params);
+        } else {
+            return this.execute(params);
+        }
+    }
+}
