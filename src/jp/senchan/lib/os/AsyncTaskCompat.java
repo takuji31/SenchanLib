60002a2426362ee3953a9381d838627f5b6b79a8
Nishibayashi Takuji <takuji@senchan.jp>
2012-06-05 16:49:39 +0900

    Support Package風なメソッド名に変更

--- src/jp/senchan/lib/os/AsyncTaskCompat.java
+++ src/jp/senchan/lib/os/AsyncTaskCompat.java
@@ -5,7 +5,7 @@ import android.os.Build;
 
 public abstract class AsyncTaskCompat<Params, Progress, Result> extends AsyncTask<Params, Progress, Result> {
 
-    public AsyncTask<Params,Progress,Result> threadExecute(Params... params) {
+    public AsyncTask<Params,Progress,Result> supportedExecute(Params... params) {
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
             return this.executeOnExecutor(THREAD_POOL_EXECUTOR, params);
         } else {
