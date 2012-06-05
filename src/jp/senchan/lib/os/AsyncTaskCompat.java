8933b4e0a0a94e34a9e8e1f0dcd198ed2de55570
Nishibayashi Takuji <takuji@senchan.jp>
2012-06-05 16:52:10 +0900

    Supportが正しい

--- src/jp/senchan/lib/os/AsyncTaskCompat.java
+++ src/jp/senchan/lib/os/AsyncTaskCompat.java
@@ -5,7 +5,7 @@ import android.os.Build;
 
 public abstract class AsyncTaskCompat<Params, Progress, Result> extends AsyncTask<Params, Progress, Result> {
 
-    public AsyncTask<Params,Progress,Result> supportedExecute(Params... params) {
+    public AsyncTask<Params,Progress,Result> supportExecute(Params... params) {
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
             return this.executeOnExecutor(THREAD_POOL_EXECUTOR, params);
         } else {
