45481ac72f81e083c727b5253b229868c6e8f6f5
Nishibayashi Takuji <takuji@senchan.jp>
2012-06-20 13:32:47 +0900

    ViewPager.OnPageChangeLisntenerを追加で使えるようにする

--- src/jp/senchan/lib/view/TabFragmentPagerAdapter.java
+++ src/jp/senchan/lib/view/TabFragmentPagerAdapter.java
@@ -13,74 +13,93 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
 
-public class TabFragmentPagerAdapter extends FragmentPagerAdapter implements ActionBar.TabListener, ViewPager.OnPageChangeListener {
-    private final Context mContext;
-    private final ActionBar mActionBar;
-    private final ViewPager mViewPager;
-    private final ArrayList<TabInfo> mTabs = new ArrayList<TabInfo>();
+public class TabFragmentPagerAdapter extends FragmentPagerAdapter implements
+		ActionBar.TabListener, ViewPager.OnPageChangeListener {
 
-    static final class TabInfo {
-        private final Class<?> clss;
-        private final Bundle args;
+	private final Context mContext;
+	private final ActionBar mActionBar;
+	private final ViewPager mViewPager;
+	private final ArrayList<TabInfo> mTabs = new ArrayList<TabInfo>();
+	OnPageChangeListener mPageChangeListener;
 
-        TabInfo(Class<?> _class, Bundle _args) {
-            clss = _class;
-            args = _args;
-        }
-    }
+	static final class TabInfo {
+		private final Class<?> clss;
+		private final Bundle args;
 
-    public TabFragmentPagerAdapter(BaseActivity<?> activity, ViewPager pager) {
-        super(activity.getSupportFragmentManager());
-        mContext = activity;
-        mActionBar = activity.getSupportActionBar();
-        mViewPager = pager;
-        mViewPager.setAdapter(this);
-        mViewPager.setOnPageChangeListener(this);
-    }
+		TabInfo(Class<?> _class, Bundle _args) {
+			clss = _class;
+			args = _args;
+		}
+	}
 
-    public void addTab(ActionBar.Tab tab, Class<?> clss, Bundle args) {
-        TabInfo info = new TabInfo(clss, args);
-        tab.setTag(info);
-        tab.setTabListener(this);
-        mTabs.add(info);
-        mActionBar.addTab(tab);
-        notifyDataSetChanged();
-    }
+	public TabFragmentPagerAdapter(BaseActivity<?> activity, ViewPager pager) {
+		super(activity.getSupportFragmentManager());
+		mContext = activity;
+		mActionBar = activity.getSupportActionBar();
+		mViewPager = pager;
+		mViewPager.setAdapter(this);
+		mViewPager.setOnPageChangeListener(this);
+	}
 
-    @Override
-    public int getCount() {
-        return mTabs.size();
-    }
+	public void setOnPageChangeListenr(OnPageChangeListener lisntener) {
+		mPageChangeListener = lisntener;
+	}
 
-    @Override
-    public Fragment getItem(int position) {
-        TabInfo info = mTabs.get(position);
-        return Fragment.instantiate(mContext, info.clss.getName(), info.args);
-    }
+	public void addTab(ActionBar.Tab tab, Class<?> clss, Bundle args) {
+		TabInfo info = new TabInfo(clss, args);
+		tab.setTag(info);
+		tab.setTabListener(this);
+		mTabs.add(info);
+		mActionBar.addTab(tab);
+		notifyDataSetChanged();
+	}
 
-    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-    }
+	@Override
+	public int getCount() {
+		return mTabs.size();
+	}
 
-    public void onPageSelected(int position) {
-        mActionBar.setSelectedNavigationItem(position);
-    }
+	@Override
+	public Fragment getItem(int position) {
+		TabInfo info = mTabs.get(position);
+		return Fragment.instantiate(mContext, info.clss.getName(), info.args);
+	}
 
-    public void onPageScrollStateChanged(int state) {
-    }
+	public void onPageScrolled(int position, float positionOffset,
+			int positionOffsetPixels) {
+		if (mPageChangeListener != null) {
+			mPageChangeListener.onPageScrolled(position, positionOffset,
+					positionOffsetPixels);
+		}
+	}
 
-    public void onTabSelected(Tab tab, FragmentTransaction ft) {
-        Object tag = tab.getTag();
-        for (int i=0; i<mTabs.size(); i++) {
-            if (mTabs.get(i) == tag) {
-                mViewPager.setCurrentItem(i);
-            }
-        }
-    }
+	public void onPageSelected(int position) {
+		mActionBar.setSelectedNavigationItem(position);
+		if (mPageChangeListener != null) {
+			mPageChangeListener.onPageSelected(position);
+		}
+	}
 
-    public void onTabUnselected(Tab tab, FragmentTransaction ft) {
-    }
+	public void onPageScrollStateChanged(int state) {
+		if (mPageChangeListener != null) {
+			mPageChangeListener.onPageScrollStateChanged(state);
+		}
+	}
 
-    public void onTabReselected(Tab tab, FragmentTransaction ft) {
-    }
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+		Object tag = tab.getTag();
+		for (int i = 0; i < mTabs.size(); i++) {
+			if (mTabs.get(i) == tag) {
+				mViewPager.setCurrentItem(i);
+			}
+		}
+	}
+
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+	}
+
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
+	}
 }
