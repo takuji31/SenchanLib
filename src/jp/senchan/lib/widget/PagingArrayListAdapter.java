4c87b37225c4aed6a6fd87a3c18743745a62e091
Nishibayashi Takuji <takuji31@gmail.com>
2012-08-30 11:23:26 +0900

    高さの調整がちゃんとできていなかった

--- src/jp/senchan/lib/widget/PagingArrayListAdapter.java
+++ src/jp/senchan/lib/widget/PagingArrayListAdapter.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import jp.senchan.lib.app.ArrayListAdapter;
 import android.content.Context;
 import android.view.View;
+import android.widget.AbsListView.LayoutParams;
 
 public abstract class PagingArrayListAdapter<T> extends ArrayListAdapter<T> {
 	
@@ -27,6 +28,12 @@ public abstract class PagingArrayListAdapter<T> extends ArrayListAdapter<T> {
 	public View createView(int position, T item, View v) {
 		View createdView = createNonLayoutedView(position, item, v);
 		if (mViewHeight != 0) {
+			LayoutParams params = (LayoutParams) createdView.getLayoutParams();
+			if (params == null) {
+				params = new LayoutParams(LayoutParams.MATCH_PARENT, mViewHeight);
+			} else {
+				params.height = mViewHeight;
+			}
 			createdView.setMinimumHeight(mViewHeight);
 		}
 		return v;
